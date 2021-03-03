94b792a995d55af3f63ed0c2f55cfa0bc7feba04
TheRealKillaruna <tobias.heimann@gmx.net>
2021-03-04 00:45:13 +0100

    Update sensor.py
    Query TouchtronicParams values by name.

--- sensor.py
+++ sensor.py
@@ -4,46 +4,52 @@ from homeassistant.helpers.entity import Entity
 from datetime import timedelta
 
 from apscheduler.schedulers.background import BackgroundScheduler
+#from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from telnetlib import Telnet
 
 class TouchtronicParams:
     def __init__(self, msg):
-        self.state              = msg[0]    # 1=Off | 5=Ignition | 6=Transition to full firing | 7=Full firing
-        self.o2                 = msg[1]
-        self.o2_target          = msg[2]
-        self.boilerTemp         = msg[3]
-        self.boilerTemp_target  = msg[4]
-        self.smokeGasTemp       = msg[5]
-        self.draft              = msg[6]
-        self.draft_target       = msg[7]
-        self.power              = msg[8]
-        self.deliveryRate       = msg[9]
-        self.currentDrawer      = msg[10]
-        self.currentGrate       = msg[13]
-        self.currentCleaning    = msg[14]
-        self.outsideTemp        = msg[15]
-        self.outsideTemp_mean   = msg[16]
-        self.bufferTemp_top     = msg[17]
-        self.bufferTemp_ctr     = msg[19]
-        self.bufferTemp_btm     = msg[21]
-        self.returnTemp         = msg[23]
-        self.returnTemp_target  = msg[24]
-        self.control_k          = msg[27]
-        self.control_f          = msg[28]
-        self.RuntimeSinceRefill = msg[32]
-        self.RuntimeSinceAshRemoval = msg[33]
-        self.ashRemovals        = msg[34]
-        self.grateMovements     = msg[35]
-        self.bufferFillRate     = msg[42]
-        self.pelletStock        = msg[46]
-        self.pelletConsumption  = msg[47]
-        self.errorCode          = msg[49]
-        self.flowTemp           = msg[56]
-        self.flowTemp_target    = msg[57]
-        self.hotWaterTemp       = msg[95]
-        self.hotWaterTemp_target= msg[96]
-        self.hotWaterPump       = msg[97]
-        self.error              = (int(msg[141],16) & 0x2000) > 0
+        self.params = {
+            "state": msg[0],    # 1=Off | 5=Ignition | 6=Transition to full firing | 7=Full firing
+            "o2" : msg[1],
+            "o2_target" : msg[2],
+            "boilerTemp" : msg[3],
+            "boilerTemp_target" : msg[4],
+            "smokeGasTemp" : msg[5],
+            "draft" : msg[6],
+            "draft_target" : msg[7],
+            "power" : msg[8],
+            "deliveryRate" : msg[9],
+            "currentDrawer" : msg[10],
+            "currentGrate" : msg[13],
+            "currentCleaning" : msg[14],
+            "outsideTemp" : msg[15],
+            "outsideTemp_mean" : msg[16],
+            "bufferTemp_top" : msg[17],
+            "bufferTemp_ctr" : msg[19],
+            "bufferTemp_btm" : msg[21],
+            "returnTemp" : msg[23],
+            "returnTemp_target" : msg[24],
+            "control_k" : msg[27],
+            "control_f" : msg[28],
+            "runtimeSinceRefill" : msg[32],
+            "runtimeSinceAshRemoval" : msg[33],
+            "ashRemovals" : msg[34],
+            "grateMovements" : msg[35],
+            "bufferFillRate" : msg[42],
+            "pelletStock" : msg[46],
+            "pelletConsumption" : msg[47],
+            "errorCode" : msg[49],
+            "flowTemp" : msg[56],
+            "flowTemp_target" : msg[57],
+            "hotWaterTemp" : msg[95],
+            "hotWaterTemp_target" : msg[96],
+            "hotWaterPump": msg[97],
+            "error" : (int(msg[141],16) & 0x2000) > 0
+        }
+            
+    def get(self, p):
+        return self.params[p]
 
 
 class TouchtronicBridge:
@@ -92,18 +98,19 @@ SCAN_INTERVAL = timedelta(seconds=5)
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the sensor platform."""
     bridge = TouchtronicBridge("192.168.0.161")
-    add_entities([NanoPKSensor(bridge, "boiler temperature", 3)])
+    add_entities([NanoPKSensor(bridge, "boiler temperature", "boilerTemp", TEMP_CELSIUS)])
 
 
 class NanoPKSensor(Entity):
     """Representation of a Sensor."""
 
-    def __init__(self, bridge, description, msgIdx):
+    def __init__(self, bridge, description, paramName, measurementUnit):
         """Initialize the sensor."""
         self._state = None
         self._bridge = bridge
         self._description = description
-        self._msgIdx = msgIdx
+        self._paramName = paramName
+        self._unit = measurementUnit
 
     @property
     def name(self):
@@ -118,12 +125,11 @@ class NanoPKSensor(Entity):
     @property
     def unit_of_measurement(self):
         """Return the unit of measurement."""
-        return TEMP_CELSIUS
+        return self._unit
 
     def update(self):
         """Fetch new state data for the sensor.
         This is the only method that should fetch new data for Home Assistant.
         """
-        p = self._bridge.getParams()
-        self._state = p.boilerTemp
+        self._state = self._bridge.getParams().get(self._paramName)
 
