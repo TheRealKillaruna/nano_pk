d27ac8a50f2970728a5a88dfdb136a1d3bd112af
TheRealKillaruna <tobias.heimann@gmx.net>
2021-03-03 10:11:26 +0100

    First working version
    Supports one temerature measurement (boiler temperature).

--- __init__.py
+++ __init__.py
@@ -1,1 +1,14 @@
 """The Hargassner Nano-PK boiler temperature sensor integration."""
+DOMAIN = 'nano_pk'
+
+
+def setup(hass, config):
+    """Your controller/hub specific code."""
+    # Data that you want to share with your platforms
+#    hass.data[DOMAIN] = {
+#        'temperature': 23
+#    }
+
+    hass.helpers.discovery.load_platform('sensor', DOMAIN, {}, config)
+
+    return True
--- sensor.py
+++ sensor.py
@@ -1,24 +1,31 @@
 """Platform for sensor integration."""
 from homeassistant.const import TEMP_CELSIUS
 from homeassistant.helpers.entity import Entity
+from datetime import timedelta
+from telnetlib import Telnet
 
+SCAN_INTERVAL = timedelta(seconds=5)
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the sensor platform."""
-    add_entities([NanoPKSensor()])
+    add_entities([NanoPKSensor("boiler temperature",3)])
 
 
 class NanoPKSensor(Entity):
     """Representation of a Sensor."""
 
-    def __init__(self):
+    def __init__(self, description, msgIdx):
         """Initialize the sensor."""
         self._state = None
+        self._connectionOK = False
+        self._tnc = Telnet()
+        self._description = description
+        self._msgIdx = msgIdx
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return 'Nano-PK boiler temperature'
+        return "Nano-PK " + self._description
 
     @property
     def state(self):
@@ -34,4 +41,20 @@ class NanoPKSensor(Entity):
         """Fetch new state data for the sensor.
         This is the only method that should fetch new data for Home Assistant.
         """
-        self._state = 23
+        if self._connectionOK:
+            try:
+                data = self._tnc.read_very_eager()
+            except EOFError as error:
+                self._connectionOK = False
+                return
+            msg = data.decode("ascii")
+            lastMsgStart = msg.rfind("pm ")
+            if lastMsgStart<0: 
+                return
+            lastMsg = msg[lastMsgStart+3:].split(' ')
+            if len(lastMsg) != 150: 
+                return
+            self._state = float(lastMsg[3])
+        else:
+            self._tnc.open("192.168.0.161")
+            self._connectionOK = True
