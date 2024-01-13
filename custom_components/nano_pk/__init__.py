0074e26479ebb2b2ea8b42102b10d7ff7d81b03d
TheRealKillaruna <tobias.heimann@gmx.net>
2024-01-13 23:22:22 +0100

    Update __init__.py
    No need to create tasks in async functions

--- custom_components/nano_pk/__init__.py
+++ custom_components/nano_pk/__init__.py
@@ -40,8 +40,6 @@ async def async_setup(hass, config) -> bool:
     hass.data[DOMAIN][CONF_LANG] = config[DOMAIN].get(CONF_LANG)
     hass.data[DOMAIN][CONF_UNIQUE_ID] = config[DOMAIN].get(CONF_UNIQUE_ID)
 
-    hass.async_create_task(
-        hass.helpers.discovery.async_load_platform('sensor', DOMAIN, {}, config)
-    )
+    await hass.helpers.discovery.async_load_platform('sensor', DOMAIN, {}, config)
 
     return True
